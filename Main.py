7e2e2afd8cb48631501f47253d0e7f8c74e40c11
VIVIN AATHAV URK21CS1154 <110010382+Viivin27@users.noreply.github.com>
2022-09-08 16:54:12 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -33,10 +33,12 @@ class Stack:
 stack = Stack()
 operations = []
 for specific_operation in input().split(','):
-    operations.append(specifi
-
+    operations.append(specific_operation.strip())
+input_data = input()
+data = input_data.split(',')
+for i in range(len(operations)):
   if operations[i] == "push":
     stack.push(int(data[i]))
- 
+  elif operations[i] == "pop":
     stack.pop()
 stack.status()
